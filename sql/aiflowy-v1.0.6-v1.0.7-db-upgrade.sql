4f4bde98ce77e651c9930a57d6c259ce48d8ec40
18586361686 <1207669650@qq.com>
2025-06-18 17:50:56 +0800

    update: 更新sql

---
+++ sql/aiflowy-v1.0.6-v1.0.7-db-upgrade.sql
@@ -0,0 +1,9 @@
+SET FOREIGN_KEY_CHECKS = 0;
+-- ----------------------------
+
+ALTER TABLE `tb_ai_knowledge` 
+ADD COLUMN `rerank_llm_id` bigint(0) NULL DEFAULT NULL COMMENT '重排模型id' AFTER `options`,
+ADD COLUMN `search_engines_enable` tinyint(1) NULL COMMENT '是否启用搜索引擎' AFTER `rerank_llm_id`;
+
+-- ----------------------------
+SET FOREIGN_KEY_CHECKS = 1;
