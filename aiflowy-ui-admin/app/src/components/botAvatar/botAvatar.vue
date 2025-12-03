9e9d9375a1839faf070fbf2b1c052119ef472706
Henry <henrycoder@qq.com>
2025-12-03 17:03:34 +0800

    feat(components): add bot avatar

---
+++ aiflowy-ui-admin/app/src/components/botAvatar/botAvatar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ElAvatar } from 'element-plus';
+
+import defaultBotAvatar from '#/assets/ai/bot/defaultBotAvatar.png';
+
+const props = defineProps<{
+  size?: number;
+  src?: string;
+}>();
+</script>
+
+<template>
+  <ElAvatar :src="props.src ?? defaultBotAvatar" :size="props.size ?? 36" />
+</template>
